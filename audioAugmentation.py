6c0215a6fc924d05b5405be4a698020d41a46ffd
Giuseppe <40702410+bibbo89@users.noreply.github.com>
2020-12-30 17:57:37 +0100

    Add files via upload

---
+++ audioAugmentation.py
@@ -0,0 +1,99 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import soundfile as sf
+
+DATASET_PATH = "test"
+SAMPLE_RATE = 22050
+STRETCH_FACTOR = 0.8
+NOISE_FACTOR = 0.009
+SHIFT_FACTOR = int(SAMPLE_RATE / 10)
+FILE_EXTENSION = '.wav'
+
+
+class AudioAugmentation:
+    def read_audio_file(self, file_path):
+        signal, sr = sf.read(file_path)
+        return signal, sr
+
+    def write_audio_file(self, file, signal, sample_rate=SAMPLE_RATE):
+        sf.write(file, signal, sample_rate)
+
+    def plot_time_series(self, signal):
+        fig = plt.figure(figsize=(14, 8))
+        plt.title('Raw wave')
+        plt.ylabel('Amplitude')
+        plt.plot(np.linspace(0, 1, len(signal)), signal)
+        plt.show()
+
+    def add_noise(self, signal, factor=NOISE_FACTOR):
+        noise = np.random.randn(len(signal))
+        data_noise = signal + factor * noise
+        return data_noise
+
+    def shift(self, signal, factor=SHIFT_FACTOR):
+        return np.roll(signal, factor)
+
+    def stretch(self, signal, factor=STRETCH_FACTOR):
+        signal = librosa.effects.time_stretch(signal, factor)
+        return signal
+
+
+def save_augment_data(dataset_path):
+    # loop inside the folder that contains all data
+    # dirpath represent the directory that contains all folder with genres
+    # dirname represent the single folders per genres
+    # filename represent the single audio sample in dirname
+    # for count a number of iteration during the for i can insert enumerate and assign to i var
+    for i, (dirpath, dirname, filenames) in enumerate(os.walk(dataset_path)):
+        # ensure that we're not at the root level
+        if dirpath is not dataset_path:
+
+            # save the semantic label in mapping label inside JSON
+            dirpath_component = dirpath.split('/')  # return a list of element
+            dirpath_root = dirpath_component[-1].split('\\')[0]
+            semantic_label = dirpath_component[-1].split('\\')[1]
+            # print('dirpath_root: ', dirpath_root)
+            # print('dirpath_component: ', dirpath_component)
+            # print('semantic_label: ', semantic_label)
+            # process files for a specific genre
+            for f in filenames:  # filenames represent only the file name and not the full path
+
+                # load audio file
+                file_path = os.path.join(dirpath, f)
+                file_component = file_path.split('/')  # return a list of element
+                file_name = file_component[-1].split('\\')[2][:-4]
+                print('file_component: ', file_component)
+                print('file_name: ', file_name)
+
+                # Create a new instance from AudioAugmentation class
+                audioaug = AudioAugmentation()
+                # Read and show cat sound
+                data, sr = audioaug.read_audio_file(file_path)
+                # print("Song:{}, Data:{} - DataLen:{} - Sample Rate:{}".format(file_path, data, len(data), sr))
+                # audioAug.plot_time_series(data)
+
+                # Adding noise to sound
+                data_noise = audioaug.add_noise(data)
+                # audioAug.plot_time_series(data_noise)
+
+                # Shifting the sound
+                data_roll = audioaug.shift(data)
+                # audioAug.plot_time_series(data_roll)
+
+                # Stretching the sound
+                data_stretch = audioaug.stretch(data)
+                # audioaug.plot_time_series(data_stretch)
+
+                # Write generated cat sounds
+                output_path = str(dirpath_root) + '/' + str(semantic_label) + '/' + file_name
+                # print('output_path: ', output_path)
+
+                audioaug.write_audio_file(output_path + '.Noise' + FILE_EXTENSION, data_noise)
+                audioaug.write_audio_file(output_path + '.Roll' + FILE_EXTENSION, data_roll)
+                audioaug.write_audio_file(output_path + '.Stretch' + FILE_EXTENSION, data_stretch)
+
+
+if __name__ == "__main__":
+    save_augment_data(DATASET_PATH)
---
+++ extractMFCC.py
@@ -0,0 +1,67 @@
+import os
+import librosa
+import math
+import json
+
+DATASET_PATH = "test"
+JSON_PATH = "features_json/DataSet_13MFCC.json"
+SAMPLE_RATE = 22050
+DURATION = 30  # measured in seconds
+SAMPLE_PER_TRACK = SAMPLE_RATE * DURATION
+MONO = True
+
+
+def save_mfcc(dataset_path, json_path, num_mfcc=13, num_fft=2048, hop_length=512, num_segments=5):
+    # dictionary to store data
+    data = {
+        "mapping": [],  # list of musical genrse
+        "mfcc": [],  # input data extract from audio sample
+        "labels": []  # target label
+    }
+
+    num_sample_per_segment = int(SAMPLE_PER_TRACK / num_segments)
+    expected_mfcc_vectors_per_segments = math.ceil(num_sample_per_segment / hop_length)  # 1.2 -> 2 i would like to have an integer
+
+    # loop inside the folder that contains all data
+    # dirpath represent the directory that contains all folder with genres
+    # dirname represent the single folders per genres
+    # filename represent the single audio sample in dirname
+    # for count a number of iteration during the for i can insert enumerate and assign to i var
+    for i, (dirpath, dirname, filenames) in enumerate(os.walk(dataset_path)):
+        # ensure that we're not at the root level
+        if dirpath is not dataset_path:
+
+            # save the semantic label in mapping label inside JSON
+            dirpath_component = dirpath.split("/")  # return a list of element
+            semantic_label = dirpath_component[-1].split("\\")[1]
+            data["mapping"].append(semantic_label)
+            print("\nProcessing: {}".format(semantic_label))
+
+            # process files for a specific genre
+            for f in filenames:  # filenames represent only the file name and not the full path
+
+                # load audio file
+                file_path = os.path.join(dirpath, f)
+                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE, duration=DURATION, mono=MONO)
+
+                # process segments extracting mfcc and storing data
+                for d in range(num_segments):
+                    start_sample = num_sample_per_segment * d
+                    finish_sample = start_sample + num_sample_per_segment
+
+                    mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample], sample_rate, n_mfcc=num_mfcc, n_fft=num_fft, hop_length=hop_length)
+                    mfcc = mfcc.T
+
+                    # store mfcc for segment if it has the expected length
+                    if len(mfcc) == expected_mfcc_vectors_per_segments:
+                        data["mfcc"].append(mfcc.tolist())
+                        data["labels"].append(i - 1)
+                        print("{}, segment:{}".format(file_path, d + 1))
+
+    # save MFCCs to json file
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH)
