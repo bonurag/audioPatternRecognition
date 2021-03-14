856310975f0a3b3420f484f414729cd146d96556
Giuseppe <40702410+bibbo89@users.noreply.github.com>
2021-03-14 12:12:23 +0100

    Add logic for extract feature on all dataset path

--- mfccToCsv.py
+++ mfccToCsv.py
@@ -1,55 +1,90 @@
 import librosa.display
 import numpy as np
 import os
-import tqdm.notebook
+import sys
 import pandas as pd
+from tqdm import tqdm
+
+eps = sys.float_info.epsilon
 
 # create a csv file from data
-DATASET_PATH = "genres_10"
-NUM_MFCC = 40
+DATASET_LIST = ["genres_5", "genres_10"]
+MFCC_LIST = [13, 20, 40]
 
-genre = os.listdir(DATASET_PATH)
-data = pd.DataFrame(
-    columns=["filename", "genre", "path", "tempo", "rmse", "chroma_stft", "chroma_cqt", "chroma_cens", "spec_cent",
-             "spec_bw", "spec_contrast", "rolloff", "zcr"] + [
-                "mfcc_{}".format(x) for x in range(NUM_MFCC)])
 
-filenames = []
-genres = []
-paths = []
+def energy(frame):
+    """Computes signal energy of frame"""
+    return np.sum(frame ** 2) / np.float64(len(frame))
 
-for gnr in genre:
-    for file in os.listdir(os.path.join(DATASET_PATH, gnr)):
-        filenames.append(file)
-        genres.append(gnr)
-        paths.append(os.path.join(DATASET_PATH, gnr, file))
-data["filename"] = filenames
-data["genre"] = genres
-data["path"] = paths
 
-data.head()
+def energy_entropy(frame, n_short_blocks=10):
+    """Computes entropy of energy"""
+    # total frame energy
+    frame_energy = np.sum(frame ** 2)
+    frame_length = len(frame)
+    sub_win_len = int(np.floor(frame_length / n_short_blocks))
+    if frame_length != sub_win_len * n_short_blocks:
+        frame = frame[0:sub_win_len * n_short_blocks]
 
-for i, row in tqdm.notebook.tqdm(data.iterrows(), total=1000):
-    audio, sr = librosa.load(row.path)
-    onset_env = librosa.onset.onset_strength(audio, sr=sr)
-    print('Index: ', i)
-    print('Audio: {}, SampleRate: {}'.format(row.path, sr))
+    # sub_wins is of size [n_short_blocks x L]
+    sub_wins = frame.reshape(sub_win_len, n_short_blocks, order='F').copy()
 
-    row["tempo"] = np.mean(librosa.beat.tempo(audio, sr=sr))
-    row["rmse"] = np.mean(librosa.feature.rms(audio))
-    row["chroma_stft"] = np.mean(librosa.feature.chroma_stft(audio, sr=sr))
-    row["chroma_cqt"] = np.mean(librosa.feature.chroma_cqt(audio, sr=sr))
-    row["chroma_cens"] = np.mean(librosa.feature.chroma_cens(audio, sr=sr))
-    row["spec_cent"] = np.mean(librosa.feature.spectral_centroid(audio, sr=sr))
-    row["spec_bw"] = np.mean(librosa.feature.spectral_bandwidth(audio, sr=sr))
-    row["spec_contrast"] = np.mean(librosa.feature.spectral_contrast(audio, sr=sr))
-    row["rolloff"] = np.mean(librosa.feature.spectral_rolloff(audio, sr=sr))
-    row["zcr"] = np.mean(librosa.feature.zero_crossing_rate(audio))
+    # Compute normalized sub-frame energies:
+    s = np.sum(sub_wins ** 2, axis=0) / (frame_energy + eps)
 
-    for x, j in zip(librosa.feature.mfcc(audio, sr=sr, n_mfcc=NUM_MFCC)[:NUM_MFCC], range(NUM_MFCC)):
-        row["mfcc_{}".format(j)] = np.mean(x)
+    # Compute entropy of the normalized sub-frame energies:
+    entropy = -np.sum(s * np.log2(s + eps))
+    return entropy
 
-num_files = "5000" if DATASET_PATH == "genres_5" else "10000"
-num_genres = "5GEN" if DATASET_PATH == "genres_5" else "10GEN"
-data.to_csv(str(NUM_MFCC) + "MFCC_" + num_files + "_" + num_genres + "_GTZAN.csv")
-data.head()
+
+for num_mfcc in MFCC_LIST:
+    for dataset_path in DATASET_LIST:
+        genre = os.listdir(dataset_path)
+        data = pd.DataFrame(
+            columns=["filename", "genre", "path", "tempo", "energy", "energy_entropy", "rmse", "chroma_stft", "chroma_cqt",
+                     "chroma_cens", "spec_cent",
+                     "spec_bw", "spec_contrast", "rolloff", "zcr"] + [
+                        "mfcc_{}".format(x) for x in range(num_mfcc)])
+
+        filenames = []
+        genres = []
+        paths = []
+
+        for gnr in genre:
+            for file in os.listdir(os.path.join(dataset_path, gnr)):
+                filenames.append(file)
+                genres.append(gnr)
+                paths.append(os.path.join(dataset_path, gnr, file))
+        data["filename"] = filenames
+        data["genre"] = genres
+        data["path"] = paths
+
+        data.head()
+        num_iterations = len(genre)*1000
+        for i, row in tqdm(data.iterrows(), total=num_iterations):
+            audio, sr = librosa.load(row.path)
+            # print('\nTrack: {} - Audio: {}, SampleRate: {}'.format(i, row.path, sr))
+
+            row["tempo"] = np.mean(librosa.beat.tempo(audio, sr=sr))
+            row["energy"] = np.mean(energy(audio))
+            row["energy_entropy"] = np.mean(energy(audio))
+            row["rmse"] = np.mean(librosa.feature.rms(audio))
+            row["chroma_stft"] = np.mean(librosa.feature.chroma_stft(audio, sr=sr))
+            row["chroma_cqt"] = np.mean(librosa.feature.chroma_cqt(audio, sr=sr))
+            row["chroma_cens"] = np.mean(librosa.feature.chroma_cens(audio, sr=sr))
+            row["spec_cent"] = np.mean(librosa.feature.spectral_centroid(audio, sr=sr))
+            row["spec_bw"] = np.mean(librosa.feature.spectral_bandwidth(audio, sr=sr))
+            row["spec_contrast"] = np.mean(librosa.feature.spectral_contrast(audio, sr=sr))
+            row["rolloff"] = np.mean(librosa.feature.spectral_rolloff(audio, sr=sr))
+            row["zcr"] = np.mean(librosa.feature.zero_crossing_rate(audio))
+
+            for x, j in zip(librosa.feature.mfcc(audio, sr=sr, n_mfcc=num_mfcc)[:num_mfcc], range(num_mfcc)):
+                row["mfcc_{}".format(j)] = np.mean(x)
+
+        num_files = "5000" if dataset_path == "genres_5" else "10000"
+        num_genres = "5GEN" if dataset_path == "genres_5" else "10GEN"
+
+        FILE_NAME = str(num_mfcc) + "MFCC_" + num_files + "_" + num_genres + "_GTZAN.csv"
+        SAVE_ROOT = "results/" + str(dataset_path) + '/'
+        data.to_csv(SAVE_ROOT+FILE_NAME)
+        data.head()
